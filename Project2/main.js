9bfe61e4898c35e3e132a5eecb071af2d6c10cb2
NadiaElMouldi <nadiaelmouldi@gmail.com>
2021-02-01 20:33:07 -0500

    changed scales and sizes

--- Project2/main.js
+++ Project2/main.js
@@ -1,7 +1,7 @@
 
-const width = window.innerWidth * 0.9,
+const width = window.innerWidth * 0.6,
   height = window.innerHeight * 0.7,
-  margin = { top: 20, bottom: 50, left: 60, right: 40 };
+  margin = { top: 20, bottom: 50, left: 100, right: 40 };
 
 let svg;
 
@@ -20,7 +20,7 @@ var main = d3.select("main");
 d3.csv("data_temp.csv", d3.autotype).then(data => {
   state.data = data;
   state.tags = [];
-  for (var i =0; i<state.data.length; i++){
+  for (var i = 0; i < state.data.length; i++) {
     console.log(i)
     var name = Object.values(state.data)[i].Name
     state.tags.push(name)
@@ -57,7 +57,7 @@ function draw(setGlobalState) {
     .style("border", "0 2px solid white")
     .style("height", "auto")
     .style("background-color", "rgba(158, 47, 40, 0)")
-    .attr("id", function(d){
+    .attr("id", function (d) {
       return d.Name
     })
     .on("mouseover", function (d) {
@@ -87,7 +87,7 @@ function draw(setGlobalState) {
       console.log("scroll")
     })
 
-    console.log(cell)
+  console.log(cell)
 
   const name = cell.selectAll(".name")
     .data(d => [d.Name])
@@ -98,7 +98,7 @@ function draw(setGlobalState) {
     .style("font-weight", "bolder")
     .style("height", "35px")
     .style("justify-content", "space-between")
-    .style("font-size", "25px")
+    .style("font-size", "17px")
     .style("text-align", "center")
     .style("text-transform", "capitalize")
 
@@ -110,7 +110,7 @@ function draw(setGlobalState) {
     .attr("class", "name_arabic")
     .style("padding", "5px")
     .style("font-weight", "bolder")
-    .style("font-size", "25px")
+    .style("font-size", "15px")
     .style("font-family", "arial")
     .style("font-stretch", "ultra-expanded")
 
@@ -122,7 +122,7 @@ function draw(setGlobalState) {
     .text(d => d)
     .attr("class", "intro")
     .style("font-weight", "lighter")
-    .style("font-size", "18px")
+    .style("font-size", "13px")
 
 
   var images = d3.selectAll("#Nawal")
@@ -136,58 +136,57 @@ function draw(setGlobalState) {
         .style("transform-origin", "100% 100%");
     })
 
-  
+
 
   // console.log(document.getElementById("info").getBoundingClientRect())
 
   const search_term = d3.select("#input_t")
   search_term
-  .on("keypress", function() {
-    if(65<d3.event.keyCode<90){
-      ac(this.value)
-    }
-    if(d3.event.keyCode === 13){
-      console.log("pressed")
-      var val = this.value;
-      const search_found = d3.selectAll('.cell')
-      search_found.each(function(d){
-        console.log(this.id)
-        console.log(val)
-        if (this.id == val) {
-          document.getElementById(val).scrollIntoView();
-          d3.select(this)
-          .transition()
-          .duration(200)
-          .style("background-color", "#e3ced3")
-          .style("color", "#141313")
-          .style("transform", "scale(1.1,1.1)")
-          .style("transform-origin", "50% 50%");
-        }
-      })
-    }
-  })
+    .on("keypress", function () {
+      if (65 < d3.event.keyCode < 90) {
+        ac(this.value)
+      }
+      if (d3.event.keyCode === 13) {
+        console.log("pressed")
+        var val = this.value;
+        const search_found = d3.selectAll('.cell')
+        search_found.each(function (d) {
+          console.log(this.id)
+          console.log(val)
+          if (this.id == val) {
+            document.getElementById(val).scrollIntoView();
+            d3.select(this)
+              .transition()
+              .duration(200)
+              .style("background-color", "#e3ced3")
+              .style("color", "#141313")
+              .style("transform", "scale(1.1,1.1)")
+              .style("transform-origin", "50% 50%");
+          }
+        })
+      }
+    })
 }
 
-     function ac(value) { 
-        document.getElementById('datalist').innerHTML = ''; 
-           
-         var n= state.tags.length;
-         var l=value.length; 
+function ac(value) {
+  document.getElementById('datalist').innerHTML = '';
 
-     for (var i = 0; i<n; i++) { 
-         if(((state.tags[i].toLowerCase()).indexOf(value.toLowerCase()))>-1) 
-         { 
-             var node = document.createElement("option"); 
+  var n = state.tags.length;
+  var l = value.length;
 
-             var val = document.createTextNode(state.tags[i]); 
-              node.appendChild(val); 
-  
-              document.getElementById("datalist").appendChild(node); 
-              console.log(document.getElementById('datalist'))
-             } 
-         } 
-     } 
-   
+  for (var i = 0; i < n; i++) {
+    if (((state.tags[i].toLowerCase()).indexOf(value.toLowerCase())) > -1) {
+      var node = document.createElement("option");
+
+      var val = document.createTextNode(state.tags[i]);
+      node.appendChild(val);
+
+      document.getElementById("datalist").appendChild(node);
+      console.log(document.getElementById('datalist'))
+    }
+  }
+}
+
 
 function setGlobalState(nextState) {
   state = { ...state, ...nextState };
